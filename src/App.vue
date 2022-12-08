ff44e4f396dabdce0b29b199f54f700bdc831620
Jocabed Ríos <j0c4b3d@gmail.com>
2022-12-07 22:45:05 -0600

    With algorithm witch finds descendants.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <ul>
     <li v-for="manager in managers" :key="manager.id">
       <span>{{ manager.name }}  </span>
-      <button @click="getChildren(manager.id)">children</button>
+      <button @click="aaalert([manager.id])">children</button>
       <button @click="addChild(manager.id)">+</button>
     </li>
   </ul>
@@ -16,37 +16,43 @@
 /* const manager_cost = 300
 const developver_cost = 100
 const qa_tester_cost = 5000 */
-
 export default {
-
   data() {
     return {
       id: 1,
       managers: [
         {id: 1, parent: null, name: "Manager A", devloper: true, qa_tester: true },
       ],
+      parents: []
     }
   },
   
   name: 'App',
   components: {
   },
-
   computed: {
     
   },
-
   methods: {
-    getChildren(parent_id) {
-      let children = this.managers.filter((t) => t.parent == parent_id)
-      console.log(children)
+    //It's going to be used to find total per manager
+    getDescendants(ascendants) {
+      let descendants = this.managers.filter(manager => ascendants.includes(manager.parent)).map(m => m.id)
+      if(descendants.length == 0) {
+        return descendants
+      }
+      else return [...descendants, ...this.getDescendants(descendants)]
     },
+
+    // Just to che
+    aaalert(ascendants){
+      console.log(this.getDescendants(ascendants))
+    },
+
+
     addChild(parent_id) {
       this.id ++
       this.managers.push({id: this.id, parent: parent_id, name: "Manager", devloper: true, qa_tester: true})
-      console.log(this.managers)
-      
-      // console.log(parent_id)
+      //console.log(this.managers)
     }
   }
 }
